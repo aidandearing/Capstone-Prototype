ffd55a6a1c359d88756b4a883f4ee6cdd8e14369
aidandearing <aidan.dearing@live.com>
2016-04-13 19:10:16 -0400

    Added Player prefab, started work on some of the prefabs for the home.
    Player prefab has circle collider, and rigidbody.
    Cabinets (side and corner), fridge (and freezer) both done.

---
+++ I am poor and hungry/Assets/PlayerController.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+    public float movementSpeed = 3;
+
+    private Rigidbody2D selfRigid;
+
+    // Use this for initialization
+    void Start()
+    {
+        selfRigid = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 movementVec = Vector3.zero;
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            movementVec += new Vector3(0, 1, 0);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            movementVec -= new Vector3(1, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            movementVec -= new Vector3(0, 1, 0);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            movementVec += new Vector3(1, 0, 0);
+        }
+
+        movementVec.Normalize();
+        movementVec *= movementSpeed;
+
+        selfRigid.MovePosition(transform.position + movementVec * Time.deltaTime);
+    }
+}
