e09bb6879faafb8a166d8226b66c2ab51c7e5dfe
aidandearing <aidan.dearing@live.com>
2016-04-24 03:02:06 -0400

    Added some basic physics layering
    CameraSeeThrough, everything invisible to the camera
    Camera, the camera physics layer

---
+++ I am poor and hungry/Assets/CraftingBehaviour.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CraftingBehaviour : MonoBehaviour
+{
+    public Inventory inventory;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void Interact(GameObject interactor)
+    {
+        PlayerController player = interactor.GetComponent<PlayerController>();
+
+        if (player != null)
+        {
+            if (player.handsFull)
+            {
+                inventory.AddItem(player.inventory.RetrieveItem());
+            }
+            else
+            {
+                // this is where we show the UI for recipes
+            }
+        }
+    }
+}
