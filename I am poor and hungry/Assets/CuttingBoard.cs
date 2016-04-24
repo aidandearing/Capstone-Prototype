c623b0542827d3dd11f53993814254fa4d92e46a
aidandearing <aidan.dearing@live.com>
2016-04-24 18:29:30 -0400

    Fixed DetectionBehaviour and Implemented SecurityCamera prefab
    This is how I pictured it being implemented.

---
+++ I am poor and hungry/Assets/CuttingBoard.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CuttingBoard : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void Interact(GameObject interactor)
+    {
+        PlayerController player = interactor.GetComponent<PlayerController>();
+
+        if (player != null)
+        {
+            if (player.handsFull)
+            {
+                GameObject item = player.inventory.RetrieveItem();
+                item.GetComponent<FoodUnity>().food.modifier.AddModifier(Modifier.ModType.Sliced);
+                player.inventory.AddItem(item);
+                Debug.Log(item.name + " has been sliced");
+            }
+        }
+    }
+}
--- I am poor and hungry/Assets/DetectionBehaviour.cs
+++ I am poor and hungry/Assets/DetectionBehaviour.cs
@@ -9,26 +9,23 @@ public class DetectionBehaviour : MonoBehaviour
     public float viewAngle = 90;
 
     private float viewThreshold;
-    public GameObject detectInstance;
+    private GameObject detectInstance;
 
     private bool detectedPlayer = false;
     private Collider2D playerLastCollision;
-	public Light cameraLight;
-	public bool increaseIntensity;
 
     // Use this for initialization
     void Start()
     {
         Vector3 angle = new Vector3(Mathf.Sin(viewAngle / 2 * Mathf.Deg2Rad), Mathf.Cos(viewAngle / 2 * Mathf.Deg2Rad), 0);
         viewThreshold = Vector3.Dot((transform.localToWorldMatrix * new Vector3(0, 1, 0)).normalized, (transform.localToWorldMatrix * angle).normalized);
-		detectIcon.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
         Vector3 delta = player.transform.position - transform.position;
-		//Debug.DrawLine(transform.position, transform.position + delta, new Color(255,0,0), 0.1f);
+        Debug.DrawLine(transform.position, transform.position + delta, new Color(255,0,0), 0.1f);
 
         if (delta.magnitude <= viewDistance)
         {
@@ -43,23 +40,6 @@ public class DetectionBehaviour : MonoBehaviour
                     {
                         if (detectedPlayer)
                         {
-							if (cameraLight.intensity >= 2.0f)
-							{
-								increaseIntensity = false;
-							}
-							if (cameraLight.intensity == 0.0f)
-							{
-								increaseIntensity = true;
-							}
-							if (increaseIntensity)
-							{
-								cameraLight.intensity += 0.1f;
-							}
-							if (!increaseIntensity)
-							{
-								cameraLight.intensity -= 0.1f;
-							}
-
                             OnDetectionStay(hit.collider);
                         }
                         else
@@ -102,12 +82,10 @@ public class DetectionBehaviour : MonoBehaviour
 
     void OnDetectionStart(Collider2D other)
     {
-//        if (detectInstance == null)
-//        {
-//			//detectInstance = Instantiate(detectIcon, new Vector3(player.transform.position.x, player.transform.position.y, player.transform.position.z - 2.5f), new Quaternion(-1,0,0,1)) as GameObject;
-//        }
-		detectIcon.SetActive(true);
-		cameraLight.color = Color.red;
+        if (detectInstance == null)
+        {
+            detectInstance = Instantiate(detectIcon, transform.position, new Quaternion()) as GameObject;
+        }
         SendMessage("StartDetectingPlayer", other.gameObject, SendMessageOptions.DontRequireReceiver);
 
         Debug.Log("Started detecting player");
@@ -121,13 +99,11 @@ public class DetectionBehaviour : MonoBehaviour
 
     void OnDetectionEnd(Collider2D other)
     {
-//        if (detectInstance != null)
-//        {
-//            Destroy(detectInstance);
-//        }
-		detectIcon.SetActive(false);
-		cameraLight.intensity = 2.0f;
-		cameraLight.color = Color.white;
+        if (detectInstance != null)
+        {
+            Destroy(detectInstance);
+            detectInstance = null;
+        }
         SendMessage("StopDetectingPlayer", other.gameObject, SendMessageOptions.DontRequireReceiver);
 
         Debug.Log("Ended detecting player");
