085a0b1912f5418a962af2abfe04082648215b43
aidandearing <aidan.dearing@live.com>
2016-04-23 21:55:41 -0400

    More House Work, and some inventory behaviour
    Fancier house, and some very rudimentary framework for the inventory.

---
+++ I am poor and hungry/Assets/Inventory.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Inventory : MonoBehaviour
+{
+    private ArrayList items;
+
+    // Use this for initialization
+    void Start()
+    {
+        items = new ArrayList();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void AddItem(GameObject item)
+    {
+        items.Add(item);
+    }
+
+    public GameObject RetrieveItem()
+    {
+        GameObject item = items[0] as GameObject;
+        items.RemoveAt(0);
+        return item;
+    }
+}
