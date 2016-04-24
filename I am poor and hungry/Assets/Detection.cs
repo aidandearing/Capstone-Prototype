63a0520c78b820b5611e2a3100a81b4376201e8f
matbecker <7m4tt3r7@gmail.com>
2016-04-23 20:30:29 -0400

    camera rotation and detection

---
+++ I am poor and hungry/Assets/Detection.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Detection : MonoBehaviour {
+
+	public GameObject player;
+	public float range;
+	public float angle;
+	private float threshold;
+	// Use this for initialization
+	void Start () {
+		// Rotate the forward by the angle / 2
+		// Store the dot product of the rotated vector against the forward
+		transform.Rotate(transform.forward, angle / 2);
+		Vector3 rotatedForward = transform.forward;
+		transform.Rotate(transform.forward, -angle / 2);
+		threshold = Vector3.Dot(rotatedForward,transform.forward);
+
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float v = Vector3.Dot(transform.forward, player.transform.position);
+
+		if (v > -threshold && v < threshold)
+		{
+			if (Physics2D.Raycast(transform.position,player.transform.position - transform.position,range).collider.gameObject.tag == "Player")
+			{
+				Debug.Log("detected!");
+			}
+		}
+	
+	}
+}
---
+++ I am poor and hungry/Assets/SecurityCameraRotation.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SecurityCameraRotation : MonoBehaviour {
+
+	public Quaternion startRot;
+	public Quaternion endRot;
+	public bool backward;
+	private float timer;
+	public float flipAmount = 4.0f;
+	// Use this for initialization
+	void Start () {
+		startRot = transform.rotation;
+		backward = false;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		timer += Time.deltaTime;
+		float percentageOfTime = Mathf.Clamp((Time.time % flipAmount) / flipAmount, 0.0f, 1.0f);
+		if (!backward)
+		{
+			transform.rotation = Quaternion.Slerp(startRot, endRot, percentageOfTime);
+		}
+		if (backward)
+		{
+			transform.rotation = Quaternion.Slerp(endRot, startRot, percentageOfTime);
+		}
+		if (timer > flipAmount)
+		{
+			FlipRotation();
+			timer = 0.0f;
+		}
+	}
+	void FlipRotation()
+	{
+		backward = !backward;
+	}
+}
